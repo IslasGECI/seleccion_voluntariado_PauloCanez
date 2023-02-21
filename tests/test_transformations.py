68e14e0e1b740f5a49d25f15e0dd9a73699e93f3
PauloCanez <97709930+PauloCanez@users.noreply.github.com>
2023-02-21 02:19:51 -0600

    cambio-lunes-pt2

--- tests/test_transformations.py
+++ tests/test_transformations.py
@@ -8,5 +8,16 @@ def test_add_offset():
     obtained = dt.add_offset(augend, addend)
     assert expected == obtained
 
-birds = [ "guacamaya", "ringo", "ave azul", "mirlo cuatro patas"]
-print(birds)
+birds = [ "guacamaya", "ringo", "ave azul", "mirlo cuatro patas"]  #empezamos con este arreglo para meter nombres de pájaros
+
+for val in range(0,len(birds)):    #con esta función obtenemos la primer letra de todo el string para nombre de los pájaros
+  first_letter = birds[val][0] #first letter nos da la primer letra de cada string en el arreglo birds
+  print(first_letter)
+
+for val in range(0,len(birds)): #con esta función se obtiene la primer letra de cada nombre de los pájaros
+  quit_space = birds[val].lower().split() #con split eliminamos los espacios en cada string con nombre de pajaros y lower nos permite almacenar el primer valor no nulo
+  for res in quit_space:   #finalmente, imprimo la primer letra de cada palabra de los nombre de los pajaros
+    print(res[0])
+
+# esto será todo por hoy, para crear la función de obtener el codigo para las aves, mi punto de partida es obtener 
+# las primeras letras de los nombres
